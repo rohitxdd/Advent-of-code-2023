1389d5861cff4d8c3990c3944bd23fd3a30f144c
rohitxd <rohitsharma998850@gmail.com>
2023-12-04 16:30:51 +0530

    completed Day-4 part-2

--- Day4/app.js
+++ Day4/app.js
@@ -1,20 +1,40 @@
 const fs = require("node:fs")
 
 const lines = fs.readFileSync("./input.txt", { encoding: "utf-8" }).split('\n').map(e => e.trim())
-let totalpoints = 0
+
+const map = new Map()
+
+
 lines.forEach(line => {
+    const currentCardNo = Number(line.split(':')[0].split(" ").at(-1))
+
+    if (!map.has(currentCardNo)) {
+        map.set(currentCardNo, 1)
+    }
+
     const [winningNums, Nums] = line.split(':')[1].split('|').map(e => e.trim().split(" ").filter(e => !!e))
+
     let point = 0
+
     Nums.forEach(element => {
         if (winningNums.includes(element)) {
-            if (point === 0) {
-                point = 1
-            } else {
-                point *= 2
-            }
+            point++;
         }
     });
-    totalpoints += point
+
+    for (let index = 1; index <= point; index++) {
+        const nextCardNumber = currentCardNo + index
+        if(nextCardNumber > lines.length){
+            break
+        }
+        if(map.has(nextCardNumber)){
+            map.set(nextCardNumber , map.get(nextCardNumber) + (map.get(currentCardNo)))
+        }else{
+            map.set(nextCardNumber, map.get(currentCardNo) + 1)
+        }
+    }
 })
 
-console.log(totalpoints)
+const result = Array.from(map.values()).reduce((acc, curr) => acc + curr, 0)
+
+console.log(result)
