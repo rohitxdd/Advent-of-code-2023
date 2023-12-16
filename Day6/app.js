558f52ebf0cd18376686e4d5ce96c53235281e09
Rohit Sharma <rohitsharma998850@gmail.com>
2023-12-16 23:02:45 +0530

    completed Day-6 part-1

---
+++ Day6/app.js
@@ -0,0 +1,36 @@
+const fs = require("node:fs");
+
+const [timeArr, distanceArr] = fs
+  .readFileSync("./input.txt", { encoding: "utf-8" })
+  .split("\n")
+  .map((e) =>
+    e
+      .split(":")[1]
+      .split(" ")
+      .filter((e) => e)
+      .map(parseFloat)
+  );
+
+let res = null
+
+for (let index = 0; index < timeArr.length; index++) {
+    
+    let time = timeArr[index]
+    let distance = distanceArr[index]
+    let count = 0
+
+    for (let t = 1; t < time; t++) {
+        let d = t * (time - t)
+        if(d > distance){
+            count++;
+        }
+    }
+
+    if(!res){
+        res = count
+    }else{
+        res *= count
+    }
+}
+
+console.log(res)
